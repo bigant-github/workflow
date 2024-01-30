3a60e00b6fa8551f3577ae012ba9bf5e757513be
galen <yapeng.gao@soucaigroup.com>
2024-01-30 15:00:58 +0800

    add wf

---
+++ wf/src/main/java/org/bigant/wf/Factory.java
@@ -0,0 +1,12 @@
+package org.bigant.wf;
+
+/**
+ * 工厂
+ *
+ * @author galen
+ * @date 2024/1/3014:10
+ */
+public class Factory {
+
+
+}
---
+++ wf/src/main/java/org/bigant/wf/form/Form.java
@@ -0,0 +1,12 @@
+package org.bigant.wf.form;
+
+/**
+ * 表单
+ *
+ * @author galen
+ * @date 2024/1/3014:19
+ */
+public interface Form {
+
+
+}
